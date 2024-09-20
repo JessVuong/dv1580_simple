09d92273180a5583defe8ca94a2dbb832ada359a
Jessica Vuong <04jessicav@gmail.com>
2024-09-20 19:17:22 +0200

    try 4

--- add.c
+++ add.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static int IsNum(char* str, size_t size)
+{
+  int i = 0;
+  int dot = 0;
+  
+  if(size<0)
+  {
+    return 1;
+  }
+  if(str[0] == '-'|| str[0]== '+')
+  {
+    ++i;
+  }
+
+  for(; i < size; ++i)
+  {
+    if(str[i] == '.')
+    {
+      if(dot==1)
+      {
+        return 2;
+      }
+      dot = 1;
+      continue;
+    }
+
+    if(!('0'<=str[i] && str[i]<='9'))
+    {
+      return 3;
+    }
+
+  } 
+  return 0;
+}
 
 int main(int argc, char *argv[]){
   if (argc < 3)
